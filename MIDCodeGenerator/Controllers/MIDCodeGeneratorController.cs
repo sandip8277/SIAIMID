de694c7f88191a147187f75306e8155e2da86572
Sandip Patil <sandip.patil@nitorinfotech.com>
2021-04-12 10:35:18 +0530

    #Initial commit

---
+++ MIDCodeGenerator/Controllers/MIDCodeGeneratorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MIDCodeGenerator.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIDCodeGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MIDCodeGeneratorController : ControllerBase
+    {
+        private readonly IMIDCodeGeneratorRepository _MIDCodeGeneratorRepository;
+
+        //Here add controller action
+    }
+}
---
+++ MIDCodeGenerator/Repository/MIDCodeGeneratorRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIDCodeGenerator.Repository
+{
+    public class MIDCodeGeneratorRepository: IMIDCodeGeneratorRepository
+    {
+        private IConfiguration Configuration;
+        private string _connectionString;
+        public void GenerareMIDCodes()
+        {
+            _connectionString = this.Configuration.GetConnectionString("ConnectionString");
+        }
+    }
+}
